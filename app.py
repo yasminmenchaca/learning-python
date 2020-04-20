182d266021464ed51d0b879ba2db47236b701d12
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-20 12:17:24 -0500

    string

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
 
-character_name = "John"
-character_age = "35"
-print("There once was a man named " + character_name + ", ")
-print("he was " + character_age + " years old. ")
-print("He really liked the name " + character_name + ", ")
-print("but didn't like being " + character_age + ".")
+# character_name = "Tom"
+# character_age = 50.5
+# is_male = False
+
+# print("There once was a man named " + character_name + ", ")
+# print("he was " + character_age + " years old. ")
+
+# character_name = "Mike"
+# print("He really liked the name " + character_name + ", ")
+# print("but didn't like being " + character_age + ".")
+
+# phrase = "Giraffe Academy"
+# print(phrase.replace("Giraffe", "Yasmin"))
