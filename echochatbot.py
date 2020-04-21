e60397f7c74ce6659cfb4708f2dd24937b4ad533
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-21 10:42:25 -0500

    completed lesson

---
+++ echochatbot.py
@@ -0,0 +1,12 @@
+"""In creating Echo Chatbot, you’re already implementing some patterns that you’ll use as you develop more complex chatbot programs:
+
+Greeting a user.
+Informing the user that they are interacting with a bot (and not a human).
+Instructing the user on how to interact with the bot.
+Accepting user input.
+Setting up output that is dependent on user input."""
+
+print("Hello, welcome Yasmin's chatbot!")
+print("I will repeat after you")
+stuff_to_echo = input("Enter anything: ")
+print("You said " + stuff_to_echo)
