eee72dbdce1606606d5354c69af408f18f28f19c
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-23 09:01:46 -0500

    working on functions

---
+++ functions.py
@@ -0,0 +1,37 @@
+"""In script.py, we have made a function for you called sing_song. Call this function once to see what it prints out.
+
+Now call sing_song a second time.
+"""
+
+
+def sing_song():
+    print("You may say I'm a dreamer")
+    print("But I'm not the only one")
+    print("I hope some day you'll join us")
+    print("And the world will be as one")
+
+
+# call sing_song() below:
+sing_song()
+sing_song()
+
+"""Write a function called loading_screen that prints "This page is loading..." to the console.
+Outside of the function body (unindented), call loading_screen()."""
+
+
+def loading_screen():
+    print("This page is loading...")
+
+
+loading_screen()
+
+"""Remove the indent on the second print statement. Run the file. Now what’s printed?"""
+
+
+def about_this_computer():
+    print("This computer is running on version Everest Puma")
+
+
+print("This is your desktop")
+
+about_this_computer()
