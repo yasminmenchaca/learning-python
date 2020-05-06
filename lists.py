2cdd0ac76960937018ed496ef06f4ec5de42c527
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-05-06 11:32:43 -0500

    in progress

---
+++ lists.py
@@ -0,0 +1,18 @@
+names = ['Jenny', 'Alexus', 'Sam', 'Grace']
+dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
+
+names_and_dogs_names = zip(names, dogs_names)
+print(list(names_and_dogs_names))
+
+orders = ['daisies', 'periwinkle']
+orders.append('tulips')
+orders.append('roses')
+
+print(orders)
+
+orders = ['daisy', 'buttercup', 'snapdragon', 'gardenia', 'lily']
+
+# Create new orders here:
+new_orders = orders + ['lilac', 'iris']
+# Fix the command by inserting brackets ([ and ]) so that it will run without errors.
+broken_prices = [5, 3, 4, 5, 4] + [4]
